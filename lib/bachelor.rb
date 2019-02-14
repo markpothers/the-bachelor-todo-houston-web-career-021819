20e760624a684238f57e879fe77e024cc7b53147
Mark Pothecary <markpothers@hotmail.com>
2019-02-14 21:53:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,13 +2,14 @@ require "pry"
 
 def get_first_name_of_season_winner(data, season)
   winner = ""
+  winner_array = []
   data.each do |data_season, contestant|
     if season == data_season
       contestant.each do |attribute, value|
+binding.pry
         if attribute["status"] == "winner"
           winner = attribute["name"]
           winner_array = winner.split(" ")
-binding.pry
         end
       end
     end
